484c005bc7d3529b39c45b22d71b568a38a987b2
Dipu Singh <dsingh197@gmail.com>
2022-09-10 23:29:13 +0530

    update

--- src/Components/Common/Layout.js
+++ src/Components/Common/Layout.js
@@ -1,15 +1,19 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom'
 import Header from './Header/Header'
 import Sidebar from './Sidebar/Sidebar'
 
 const Layout = ({ children }) => {
+    const location = useLocation();
+
     return (
         <>
             <Header />
             <div className='flex flex-auto'>
-                <Sidebar />
+                {/* <Sidebar /> */}
+                { ((location.pathname != '/landing') && (location.pathname != '/') && (location.pathname != '/login') && (location.pathname != '/error')) && <Sidebar />}
                 <div className='grow'>
-                    <div className='m-5'>{children}</div>
+                    <div className='m-0'>{children}</div>
                 </div>
             </div>
         </>
